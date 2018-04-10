f0ee8bbee0ef25ca499509506c3a0b045ad2e28d
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2018-04-10 16:26:00 +0200

    basic structure

--- multiqc/modules/vcpipe/vcpipe.py
+++ multiqc/modules/vcpipe/vcpipe.py
@@ -3,13 +3,28 @@
 """MultiQC module to parse output from OUS variant calling pipeline"""
 
 from __future__ import print_function
-from collections import OrderedDict
+# from collections import OrderedDict
 import logging
 import json
-from multiqc import config
 from multiqc.modules.base_module import BaseMultiqcModule
 
 log = logging.getLogger(__name__)
 
+
 class MultiqcModule(BaseMultiqcModule):
-    pass
+
+    def __init__(self):
+
+        super(MultiqcModule, self).__init__(
+            name='vcpipe',
+            anchor='vcpipe',
+            href='https://git.ousamg.io/apps/vcpipe',
+            info='Variant calling pipeline in Oslo University Hospital'
+        )
+
+        for f in self.find_log_files("vcpipe", filehandles=True):
+            self.mod_data[f['s_name']] = self.parse_log(f)
+            self.add_data_source(f)
+
+    def parse_log(log_fh):
+        data = json.load(log_fh)
