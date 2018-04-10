119b930c4282b0d039d9924e7e133a91916eb39f
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2018-04-10 12:48:37 +0200

    initial OUS vcpipe commit

---
+++ multiqc/modules/vcpipe/vcpipe.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""MultiQC module to parse output from OUS variant calling pipeline"""
+
+from __future__ import print_function
+from collections import OrderedDict
+import logging
+import json
+from multiqc import config
+from multiqc.modules.base_module import BaseMultiqcModule
+
+log = logging.getLogger(__name__)
+
+class MultiqcModule(BaseMultiqcModule):
+    pass
